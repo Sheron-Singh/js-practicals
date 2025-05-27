3bb74aca7a7b290fd7c07d025bcca7d29c96ff8c
Sheron-Singh <109852734+Sheron-Singh@users.noreply.github.com>
2025-05-27 08:01:34 +0000

    variables and dataTypes

---
+++ basic_concept/data_types.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as newer version
+
+// alert MDN documentation
+// ******** data type and ECMA standards
+
+// alert(3 + 3); // we are using nodejs, not browser 
+
+let name = "sheron";
+let age = 20;
+let isLoggedIn = false;
+
+// Data Types 
+// number
+// bigInt 
+// String
+// boolean
+// Null => standalone value
+// undefined
+// symbol => unique
+
+console.log(typeof "Sheron");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ basic_concept/variables.js
@@ -0,0 +1,19 @@
+const accountId = 14
+let accountEmail = "singh@gmail.com"
+var accountPwd ="123456"
+
+accountCity =" jaipur"
+
+let accountState;
+
+// accountId =2;  
+accountEmail ="sheron@gmail.com"
+accountPwd = "12345"
+accountCity = "Bangaluru"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPwd, accountCity,accountState])
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
